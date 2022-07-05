b8cb295509a7edb9260c6a9273822f6f154b9282
FlaCrow <kyrbix@gmail.com>
2022-07-05 15:47:12 +0300

    Added Bottom Navigation

--- app/src/main/java/com/flacrow/showtracker/presentation/MainActivity.kt
+++ app/src/main/java/com/flacrow/showtracker/presentation/MainActivity.kt
@@ -2,6 +2,10 @@ package com.flacrow.showtracker.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.flacrow.showtracker.R
 import com.flacrow.showtracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +17,30 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        val navController = binding.navHostFragmentContainer.findNavController()
+        binding.bottomNav.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.movieDetailsFragment -> hideBottomNav()
+                R.id.seriesDetailsFragment -> hideBottomNav()
+                else -> showBottomNav()
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+    binding.bottomNav.isVisible = true
+    }
+
+    private fun hideBottomNav() {
+        binding.bottomNav.isVisible = false
     }
 }
